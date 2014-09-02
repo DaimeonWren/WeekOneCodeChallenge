723bec2d3d1cbe677a1395bc2e2d4ef144a7004b
Daimeon DD Wren <daimeonddwren@gmail.com>
2014-09-02 16:14:58 -0600

    What I've done in Week1CodeChallenge

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+            { 
+                // don't know how to run both for loops at th same time, but i do know they work, i would have to delete one of the for loops.
+                FizzBuzz(i);
+            }
+
+           for (int i = 92; i >= 79; i--)
+            {
+                FizzBuzz(i);
+            }
+           Yodaizer("I like code");
+
+            Console.ReadKey();
+        }
+
+        static void FizzBuzz(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+        }
+
+        static void Yodaizer(string text)
+        {
+
+            List<string> looseGoose = text.Split(' ').ToList();
+           // i create a list of strings and split the text where there are spaces then .Tolist
+             List<string> Re = new List<string>();
+            // i create an empty sting within Re so i may add anything i'd like
+            bool isThree = looseGoose.Count == 3;
+            // the word count of looseGoose is 3 
+
+            if (isThree)
+           // Then create an "if" statement for the bool
+            {
+               //I use the .Add method to the rearange the text with index[]
+                Re.Add(looseGoose[2]);
+                Re.Add(looseGoose[0]);
+                Re.Add(looseGoose[1]);
+            }
+                // Then create an else statement for a "for" loop to run the code through it backward
+            else 
+            // opptional else statement if 
+            {
+                for (int i = looseGoose.Count - 1; i >= 0; i--)
+                {
+                   //looseGoose.count
+                    Re.Add(looseGoose[i]);
+                    // 
+                     
+                    {
+                        
+                    }
+                }
+                
+            }
+            string output = string.Join(" ", Re);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Week1CodeChallengeClass-4/Week1CodeChallengeClass-4/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1CodeChallengeClass_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Yodazizer("I Like Code");
+            TextStats("abcdefghijklmnopqrstuvwxyz");
+            TextStats("HAHAHAHAHAHAHA dodosmfaspfmasdpfmad[fnasdoufhp9");
+            for (int i = 1; i < 25; i++)
+            {
+                IsPrime(i);
+            }
+            DashInsert(8675309);
+            
+            Console.ReadKey();
+        }
+
+        static void FizzBuzz(int number)
+        {
+
+        }
+
+        static void Yodazizer(string text)
+        {
+            List<string> words = text.Split(' ').ToList();
+
+            string output = "";
+
+            for (int i = words.Count - 1; i >= 0; i--)
+            {
+                output = output + words[i] + " ";
+            }
+            Console.WriteLine(output);
+        }
+
+        static void TextStats(string input)
+        {
+            int numChars = input.Length;
+            int numWords = input.Split(' ').ToList().Count;
+            int numVowels = 0;
+            int numCons = 0;
+            int numSpec = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                // grab our current letter from the text
+                string letter = input[i].ToString();
+                if ("aeiou".Contains(letter.ToLower()))
+                {
+                    numVowels++;
+                }
+                else if (".,?".Contains(letter))
+                {
+                    numSpec++;
+                }
+                else
+                {
+                    numCons++;
+                }
+
+            }
+            Console.WriteLine(input);
+            Console.WriteLine("# Chars " + numChars);
+            Console.WriteLine("# Words " + numWords);
+            Console.WriteLine("# Vowels " + numVowels);
+            Console.WriteLine("# Consonants " + numCons);
+            Console.WriteLine("# Spec Chars " + numSpec);
+
+
+        }
+
+        static void IsPrime(int number)
+        {
+            // need to check all numbers in between 1 and itself to see if it is divisible by anything else
+            // assume the number is prime until it isn't
+            bool prime = true;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    prime = false;
+                    break; // escape the loop
+                }
+            }
+            if (prime)
+            {
+                Console.WriteLine(number + " is prime");
+            }
+
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void DashInsert(int number) 
+        {
+        // Goal Insert a dash between two odd numbers
+            //decare a string to hold our number as a string
+            string output = "";
+            string numberString = number.ToString();
+            //loop through each digit of our string
+            for (int i = 0; i <= numberString.Length - 2; i++)
+            {
+                // get our current digit and the one next to it
+                string num1String = numberString[i].ToString();
+                string num2String = numberString[i + 1].ToString();
+                //convert our numberStrings to actual numbers
+                int num1 = int.Parse(num1String);
+                int num2 = int.Parse(num2String);
+                if (num1 % 2 != 0 && num2 % 2 != 0)
+                {
+                    // both odd, add to the output
+                    output = output + num1 + "-";
+                }
+                else 
+                {
+                    output = output + num1;
+                }
+            }
+          //need to add the last digits to our output, because the loop does not add it
+            output = output + numberString[numberString.Length - 1];
+            Console.WriteLine(output);
+        }
+    
+    }
+} 
